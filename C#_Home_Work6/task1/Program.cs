5fad4403f507d53d25eadb702f9aa323e48e6675
Katerina <oeu89@mail.ru>
2022-07-13 14:35:26 +0300

    Add folders task1-2

---
+++ C#_Home_Work6/task1/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+int GetQuantity()
+{
+    int result = 0;
+    Console.WriteLine("Введите количество чисел");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не натурально число. Введите корректное число ");
+        }
+    }
+    return result;
+}
+
+
+int GetNumber()
+{
+    int result = 0;
+    Console.WriteLine("Введите числo");
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не натурально число. Введите корректное число ");
+        }
+    }
+
+    return result;
+}
+
+int quantity = GetQuantity();
+
+int result = 0;
+
+for (int i = 1; i <= quantity; i++)
+{
+    int number = GetNumber();
+    if (number > 0)
+        result ++;
+}
+
+Console.WriteLine($"Количество чисел больше 0 = {result}");
---
+++ C#_Home_Work6/task2/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)*/
+
+double GetNumber(string message)
+{
+    double result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            if (result == 0)
+            {
+                Console.Clear();
+                Console.WriteLine(errorMessage);
+            }
+            else
+                break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+
+void GetXY(double k1, double b1, double k2, double b2)
+{
+    double x = 0;
+    double y = 0;
+    if (k1 == k2)
+    {
+               Console.WriteLine($"Прямые параллельны параллельны друг другу, точек пересечения нет.");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2 = ({x}, {y})");
+    }
+}
+double k1 = GetNumber("Введите коэффициент k1");
+double b1 = GetNumber("Введите коэффициент b1");
+double k2 = GetNumber("Введите коэффициент k2");
+double b2 = GetNumber("Введите коэффициент b2");
+
+GetXY(k1, b1, k2, b2);
+
+
