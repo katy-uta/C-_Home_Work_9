34998d6db714dcc72e082bb0faa97bbb5d94176b
Katerina <oeu89@mail.ru>
2022-07-19 23:17:47 +0300

    Add task1-3

---
+++ C#_Home_Work7/task2/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+double[,] InitMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(100);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void GetPosition(int row, int column, double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == row && column == j)
+                Console.WriteLine($"Число с позицией [{row},{column}] = {matrix[i, j]} ");
+        }
+    }
+
+
+}
+
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+double[,] matrix = InitMatrix(m, n);
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine("Введите индекс строки искомого числа матрицы:");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца искомого числа матрицы:");
+int column = int.Parse(Console.ReadLine());
+if (row >= 0 && row <= m & column >= 0 && column <= n)
+    GetPosition(row, column, matrix);
+else
+    Console.WriteLine($"Числа с позицией [{row},{column}] в массиве нет ");
---
+++ C#_Home_Work7/task3/Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1,10);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void GetAverage(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        double average = 0;
+        for (int j = 0; j < m; j++)
+        {
+            average = average + matrix[j, i];
+
+        }
+        double result = average / m;
+        Console.WriteLine($"Среднее арифметическое {i + 1} столбца = {result} ");
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+int[,] matrix = InitMatrix(m, n);
+Console.WriteLine($"Матрица размером {m}x{n}:");
+Console.WriteLine();
+PrintMatrix(matrix);
+GetAverage(matrix, m, n);
